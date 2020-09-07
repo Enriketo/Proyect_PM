42d3bdb4b1b4d2a5b9d2d43b89e5fd98511687a7
Enriketo <anaelmarin86@gmail.com>
2020-09-06 19:25:26 -0500

    Changes 3

---
+++ Backend/routes/users.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const UsersService = require('../services/users');
+
+const {
+  userIdSchema,
+  createUserSchema,
+  updateUserSchema
+} = require('../utils/schemas/users');
+
+const validationHandler = require('../utils/middleware/validationHandler');
+
+function usersApi(app) {
+  const router = express.Router();
+  app.use('/api/users', router);
+
+  const usersService = new UsersService();
+
+  router.get('/', async function(req, res, next) {
+    const { tags } = req.query;
+
+    try {
+      const users = await usersService.getUsers({ tags });
+
+      res.status(200).json({
+        data: users,
+        message: 'users listed'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.get(
+    '/:userId',
+    validationHandler({ userId: userIdSchema }, 'params'),
+    async function(req, res, next) {
+      const { userId } = req.params;
+
+      try {
+        const users = await usersService.getUser({ userId });
+
+        res.status(200).json({
+          data: users,
+          message: 'user retrieved'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+
+  router.post('/', validationHandler(createUserSchema), async function(
+    req,
+    res,
+    next
+  ) {
+    const { body: user } = req;
+    try {
+      const createdUserId = await usersService.createUser({ user });
+
+      res.status(201).json({
+        data: createdUserId,
+        message: 'user created'
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.put(
+    '/:userId',
+    validationHandler({ userId: userIdSchema }, 'params'),
+    validationHandler(updateUserSchema),
+    async function(req, res, next) {
+      const { userId } = req.params;
+      const { body: user } = req;
+
+      try {
+        const updatedUserId = await usersService.updateUser({
+          userId,
+          user
+        });
+
+        res.status(200).json({
+          data: updatedUserId,
+          message: 'user updated'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+
+  router.delete(
+    '/:userId',
+    validationHandler({ userId: userIdSchema }, 'params'),
+    async function(req, res, next) {
+      const { userId } = req.params;
+
+      try {
+        const deletedUserId = await usersService.deleteUser({ userId });
+
+        res.status(200).json({
+          data: deletedUserId,
+          message: 'user deleted'
+        });
+      } catch (err) {
+        next(err);
+      }
+    }
+  );
+}
+
+module.exports = usersApi;
